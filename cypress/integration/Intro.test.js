b0507e7e4fc2fe764c99faf89f19e73951ad8003
danielambos <daniel.ambos@fdd.com.br>
2019-11-28 02:18:01 -0300

    intro tests

---
+++ cypress/integration/Intro.test.js
@@ -0,0 +1,14 @@
+/* globals cy */
+
+describe('Intro', () => {
+	beforeEach(() => {
+		cy.visit('localhost:3000')
+	});
+
+	it('should enter Name and submit form', () => {
+		// fill name input
+		cy.get('input[name="name"]').type('Test{enter}')
+		// shoul redirect to library page
+		cy.url().should('match', new RegExp('localhost:3000/library$'))
+	})
+})
